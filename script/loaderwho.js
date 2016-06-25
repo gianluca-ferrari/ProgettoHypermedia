76f69c2d358e9ad42c8d81bb55e59d101743b5c1
Andrea Facchini <andrea.facchini.io@gmail.com>
2016-06-25 12:10:30 +0200

    who we are

--- script/loaderwho.js
+++ script/loaderwho.js
@@ -1,5 +1,5 @@
 $(document).ready(loadWho());
-$(document).ready(loadTTestimonials());
+$(document).ready(loadTestimonials());
 
 function loadWho() {
   var xhttp=new XMLHttpRequest();
@@ -16,7 +16,7 @@ function loadTestimonials() {
   var xhttp=new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
-     document.getElementById("movingtextgroup").innerHTML = xhttp.responseText;
+     document.getElementById("teste").innerHTML = parseCategories(xhttp.responseText);
     }
   };
   xhttp.open("GET", "../php/whoweare.php", true);
@@ -27,16 +27,18 @@ function loadTestimonials() {
 function parseCategories(response) {
     var arr = JSON.parse(response);
     var i;
-    var out = "";
+    var out = "<div class=\"col-xs-3\"></div>";
     for(i = 0; i < arr.length; i++) {
-
-       out += "<div class=\"testimonialsimg \">";
-        out += "<img src=" + arr[i].img + "\" class=\"img-responsive\">";
+        out +="<div class=\"col-xs-2\">";
+        out += "<div >";
+        out += "<img src=\"" + arr[i].img + "\" class=\"testimonialsimg\">";
         out += "</div>";
-         out += "<div class=\"testimonialsquote\">" + arr[i].frase + "</div>";        
-        out += "<div class=\"testimonialsname\">" + arr[i].nome + "</div>";
-        
-        return out;
+        out += "<div class=\"testimonialsquote\">" + arr[i].frase + "</div>";        
+        out += "<div class=\"testimonialsname\">- " + arr[i].nome + "</div>";   
+        out += "</div>";
     }
+    out +="<div class=\"col-xs-3\"></div>";
+     return out;
+
 }
     
