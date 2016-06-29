383c2788fba0ad8215abe1273ea184d5004a371d
Diego <diego.gaboardi@alice.it>
2016-06-29 14:16:32 +0200

    header and footer

--- script/header.js
+++ script/header.js
@@ -4,6 +4,7 @@ function loadHeader(position) {
     xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
       document.getElementById('header').innerHTML = createHeader(position);
+      document.getElementById('footer').innerHTML = createFooter();
       }
    };
     xhttp.open("GET", "../php/category.php", true);
@@ -25,7 +26,7 @@ function createHeader(position)
         cl5=" " +" class='active' ";
     
      var out="";
-        out += "<nav class=\"navbar navbar-default col-sm-12\">";
+        out += "<nav class=\"navbar navbar-default navbar-fixed-top col-sm-12\">";
         out += "          <div class=\"container-fluid\">";
         out += "            <div class=\"navbar-header\">";
         out += "              <a class=\"navbar-brand\" href=\"#\"><a href=index.html><img src=\"..\/img\/tim_logo.png\" class=\"logo\"><\/a><\/a>";
@@ -39,5 +40,29 @@ function createHeader(position)
         out += "            <\/ul>";
         out += "          <\/div>";
         out += "        <\/nav>";
+    return out;
+}
+
+function createFooter()
+{
+    var out="";
+        out += "<nav class=\"navbar navbar-fixed-bottom footer col-sm-12\"";
+        out += "      <div class=\"container-fluid\">";
+        out += "        <ul class=\"nav navbar-nav\">";
+        out += "          <li><img class=\"img-responsive\" src=\"..\/img\/tim_logo.png\" alt=\"logo tim\" style=\"widht:154px; \"><\/li>";
+        out += "          <li><a href=\"contatti.html\">Contatti<\/a><\/li>";
+        out += "          <li><a href=\"group.html\">The Group<\/a><\/li> ";
+        out += "        <\/ul>";
+        out += "";
+        out += "        <div class=logos>";
+        out += "            <i class=\"fa fa-facebook-official\" style=\"font-size:36px\"><\/i>";
+        out += "            <i class=\"fa fa-google-plus-square\" style=\"font-size:36px\"><\/i>";
+        out += "            <i class=\"fa fa-twitter\" style=\"font-size:36px\"><\/i>";
+        out += "            <i class=\"fa fa-youtube\" style=\"font-size:36px\"><\/i>";
+        out += "            <i class=\"fa fa-linkedin-square\" style=\"font-size:36px\"><\/i>";
+        out += "            <p>Telecom Italia 2016 - P.IVA 00488410010<\/p>";
+        out += "        <\/div>";
+        out += "    <\/div>";
+        out += "<\/nav>";
     return out;
 }
